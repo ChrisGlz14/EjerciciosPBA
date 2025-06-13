e54ebef3537942db2ac9a6ef8368edc08076277d
Chrisglz14 <Christiang14@hotmail.com.ar>
2025-06-13 05:50:54 -0300

    Agrego packages de Guias

--- src/guiaEjercicioU4/mainU4.java
+++ src/guiaEjercicioU4/mainU4.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package guiaEjercicio4;
+package guiaEjercicioU4;
 
 /**
  *
